6903d4f8a80aeea69e3ef29a01bf4d0092ef084c
Nick Pettis <pettisnick@gmail.com>
2020-11-11 15:20:43 -0500

    home page

--- Main/client/src/components/Form/index.js
+++ Main/client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="Title, Keywords, Author"
           name="q"
           onChange={handleInputChange}
           required
