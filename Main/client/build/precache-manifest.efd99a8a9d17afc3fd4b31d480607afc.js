13ffe06b86b0e667e1becc5a8a40b355fd7afc35
Nick Pettis <pettisnick@gmail.com>
2020-11-10 22:59:48 -0500

    components

---
+++ Main/client/build/precache-manifest.efd99a8a9d17afc3fd4b31d480607afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f628d8654ff5e712ae733166594a420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "043e5132a32ef7e8bc8b",
+    "url": "/static/css/main.174b53f3.chunk.css"
+  },
+  {
+    "revision": "b54b35988f847c8fb4de",
+    "url": "/static/js/2.7bf1cf89.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.7bf1cf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043e5132a32ef7e8bc8b",
+    "url": "/static/js/main.7354d3e6.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
